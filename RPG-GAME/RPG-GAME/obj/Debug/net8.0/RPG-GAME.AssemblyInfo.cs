15c652c9011e815f88eab7137f058255cb1dae1b
Solti Csongor Péter <solti.csongor.peter@gmail.com>
2025-03-23 13:36:57 +0100

    asd

--- RPG-GAME/RPG-GAME/obj/Debug/net8.0/RPG-GAME.AssemblyInfo.cs
+++ RPG-GAME/RPG-GAME/obj/Debug/net8.0/RPG-GAME.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG-GAME")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13f05e8985207a79ee311965a9afbcd72accc393")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfeacc777c67f6f29a060cb0380edc45659d09f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG-GAME")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG-GAME")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
