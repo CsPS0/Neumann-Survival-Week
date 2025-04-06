98134bd9889962dde25ce36a70b12b0a4c08b513
Lobster <polyak.daavid@gmail.com>
2025-04-06 12:02:54 +0200

    AssetLib init

---
+++ RPG-GAME/AssetsLib/Asset.cs
@@ -0,0 +1,28 @@
+﻿namespace AssetsLib
+{
+    public class Asset
+    {
+        public static readonly string AssetPath =
+            Directory.GetParent(Directory.GetCurrentDirectory())
+            .Parent.Parent.Parent.FullName + "\\Assets";
+
+        public static Dictionary<string, object?>? GetFileTree(string path)
+        {
+            if (!Directory.Exists(path)) return null;
+
+            Dictionary<string, object?>? tree = new();
+
+            foreach (string file in Directory.GetFiles(path))
+            {
+                tree.Add(file, null);
+            }
+
+            foreach (string dir in Directory.GetDirectories(path))
+            {
+                tree.Add(dir, GetFileTree(dir));
+            }
+
+            return tree;
+        }
+    }
+}
