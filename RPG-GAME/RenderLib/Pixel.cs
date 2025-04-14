c9105209a4aaf64b7a4fe3a3af7e3559565c9495
Lobster <polyak.daavid@gmail.com>
2025-04-14 19:38:49 +0200

    feature: Pixel.FromString()

--- RPG-GAME/RenderLib/Pixel.cs
+++ RPG-GAME/RenderLib/Pixel.cs
@@ -21,8 +21,28 @@
             if (a == null || b == null) return false;
             return a.character == b.character && a.fg.Equals(b.fg) && a.bg.Equals(b.bg);
         }
+        public static Pixel? FromString(string pixel)
+        {
+            string[] parts = pixel.Split(',');
+            if (parts.Length == 8 &&
+                char.TryParse(parts[0], out char ch) &&
+                byte.TryParse(parts[1], out byte r1) &&
+	            byte.TryParse(parts[2], out byte g1) &&
+	            byte.TryParse(parts[3], out byte b1) &&
+	            byte.TryParse(parts[4], out byte r2) &&
+	            byte.TryParse(parts[5], out byte g2) &&
+	            byte.TryParse(parts[6], out byte b2) &&
+	            int.TryParse(parts[7], out int layer))
+            {
+                var fg = (r1, g1, b1);
+                var bg = (r2, g2, b2);
+                return new(ch, fg, bg, layer);
+            }
 
-        public override string ToString() 
-            => $"{character},{fg},{bg},{layer}";
+            return null;
+        }
+
+        public override string ToString()
+            => $"{character},{fg.r},{fg.g},{fg.b},{bg.r},{bg.g},{bg.b},{layer}";
     }
 }
