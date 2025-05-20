e39489a26f7063ff5c09f83cde4509ba4dd694c4
Solti Csongor Péter <solti.csongor.peter@gmail.com>
2025-05-20 17:08:16 +0200

    💩

--- RPG-GAME/RPG-GAME/Program.cs
+++ RPG-GAME/RPG-GAME/Program.cs
@@ -108,7 +108,7 @@ walk2_frame.RaplacePixels((int x, int y, Pixel? p) =>
 GameObjectsLib.Object test = new(20, 20);
 test.animation_fps = 5;
 test.animations.Add("walk", [ walk1_frame, walk2_frame ]);
-Game.OnRender += () => test.PlayAnimation("walk");
+Game.OnRender += () => test.PlayAnimation();
 // this is soo good and easy though
 
 
